02ede17332620a9f3a5f098f4ac1a600e9a8e07b
Jack Maloney <jmmaloney4@gmail.com>
2018-06-03 17:42:59 -0500

    Add PromiseKit as dependency, might use later

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0"),
         .package(url: "https://github.com/ClibSwift/ncurses.git", .branch("master")),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "17.0.0"),
+        .Package(url: "https://github.com/mxcl/PromiseKit", from: "6.2.0")
         
         /*
         
