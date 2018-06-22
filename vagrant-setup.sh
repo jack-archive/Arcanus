cea8db71b2c063bd8f0e1d8ff677181995006070
Jack Maloney <jmmaloney4@gmail.com>
2018-06-21 21:56:47 -0500

    Create vagrant-setup.sh

---
+++ vagrant-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install git clang libncurses-dev  libcurl4-openssl-dev openssl libssl-dev uuid-dev
+
+git clone https://github.com/kylef/swiftenv.git ~/.swiftenv
+echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bashrc
+echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
+
+source ~/.bashrc
+
+swiftenv install 4.1.2
+
+git clone https://github.com/jmmaloney4/Arcanus.git
+cd Arcanus
+swift build
