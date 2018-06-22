416e686d45cff7856fe570e47e15cb8c160e2a6e
Jack Maloney <jmmaloney4@gmail.com>
2018-06-22 10:44:58 -0500

    Updates

--- Sources/Arcanus/Server/Server.swift
+++ Sources/Arcanus/Server/Server.swift
@@ -6,6 +6,8 @@
 
 import PerfectHTTP
 import PerfectHTTPServer
+import Dispatch
+import PerfectLib
 
 let apiRoutes = Route(method: .get, uri: "/ping", handler: {
     request, response in
@@ -17,8 +19,58 @@ let apiRoutes = Route(method: .get, uri: "/ping", handler: {
 public func ServerMain() {
     log.warning("*** Starting Server ***")
     do {
-        try HTTPServer.launch(.server(name: "Arcanus", port: 8181, routes: [apiRoutes]))
+        GameServer.shared.start()
     } catch {
         fatalError()
     }
+}
+
+public class GameServer {
+ 
+    // Singleton, lazy
+    public static var shared = GameServer()
+    
+    var queue: DispatchQueue
+    var games: [Game] = []
+    
+    init() {
+        queue = DispatchQueue(label: "Arcanus Game Server")
+        
+        var routes = Routes()
+        routes.add(method: .get, uri: "/games/", handler: getGames)
+        
+        do {
+            try HTTPServer.launch(.server(name: "Arcanus", port: 8181, routes: [apiRoutes]))
+            log.info("Launched")
+        } catch {
+            
+        }
+    }
+    
+    func start() {
+        
+    }
+    
+    func getGames(req: HTTPRequest, res: HTTPResponse)  {
+        var json = "[\(games.map({ $0.state.description }).joined(separator: ", "))]"
+        res.appendBody(string: json)
+    }
+}
+
+public class Game {
+    public enum State: CustomStringConvertible {
+        case waitingForPlayers
+        case running
+        case finished
+        
+        public var description: String {
+            switch self {
+            case .waitingForPlayers: return "Waiting for Players"
+            case .running: return "Running"
+            case .finished: return "Finished"
+            }
+        }
+    }
+    
+    var state: State = .waitingForPlayers
 }
--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install git clang libncurses-dev  libcurl4-openssl-dev openssl libssl-dev uuid-dev
+sudo apt-get update
+sudo apt-get install -y git clang libncurses-dev  libcurl4-openssl-dev openssl libssl-dev uuid-dev sqlite3
 
 git clone https://github.com/kylef/swiftenv.git ~/.swiftenv
 echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bashrc
 echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(swiftenv init -)"' >> ~/.bashrc
+
+export SWIFTENV_ROOT="$HOME/.swiftenv"
+export PATH="$SWIFTENV_ROOT/bin:$PATH"
+eval "$(swiftenv init -)"
 
 source ~/.bashrc
 
