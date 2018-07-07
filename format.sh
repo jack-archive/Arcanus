7d2d78d506658405218fc68f120c14157455272e
Jack Maloney <jmmaloney4@gmail.com>
2018-07-06 22:14:47 -0500

    Add format.sh

---
+++ format.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+swiftformat --allman false \
+    --wraparguments afterfirst \
+    --wrapelements afterfirst \
+    --self insert \
+    --header ignore \
+    --binarygrouping 4,8 \
+    --octalgrouping none \
+    --indentcase false \
+    --trimwhitespace always \
+    --decimalgrouping 3,6 \
+    --patternlet hoist \
+    --commas inline \
+    --semicolons inline \
+    --indent 4 \
+    --exponentcase lowercase \
+    --operatorfunc spaced \
+    --elseposition same-line \
+    --empty tuple \
+    --ranges nospace \
+    --hexliteralcase uppercase \
+    --linebreaks lf \
+    --hexgrouping none \
+    --comments ignore \
+    --ifdef noindent \
+    --stripunusedargs closure-only .
