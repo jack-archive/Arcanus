1041a9e3c60248ee9631f4cb54156054384698e5
Jack Maloney <jmmaloney4@gmail.com>
2018-08-21 11:58:49 -0500

    GET/DELETE decks

--- Sources/Arcanus/Routes/DeckRouteController.swift
+++ Sources/Arcanus/Routes/DeckRouteController.swift
@@ -8,10 +8,20 @@ import Fluent
 import Foundation
 import Vapor
 
+struct DeckIDParam: Parameter {
+    static func resolveParameter(_ parameter: String, on container: Container) throws -> ResolvedParameter {
+        return try Deck.ID(parameter).unwrap(or: Abort(.unprocessableEntity, reason: "\(parameter) is not convertable to Int"))
+    }
+
+    typealias ResolvedParameter = Deck.ID
+}
+
 class DeckRouteController: RouteCollection {
     func boot(router: Router) throws {
         router.post("user", "collection", use: createDeckHandler)
         router.get("user", "collection", use: getAllDecksHandler)
+        router.get("user", "collection", DeckIDParam.parameter, use: getDeckHandler)
+        router.delete("user", "collection", DeckIDParam.parameter, use: deleteDeckHandler)
     }
 }
 
@@ -41,5 +51,31 @@ private extension DeckRouteController {
             .map { decks in
                 decks.map { deck in return deck.toNameJson() }
             }
+    }
+
+    func getDeckHandler(_ request: Request) throws -> Future<Deck.NameJson> {
+        let user = try request.requireAuthenticated(User.self)
+        let id = try request.parameters.next(DeckIDParam.self)
+
+        return Deck.find(id, on: request).map { deck in
+            guard deck != nil, try deck!.user == user.requireID() else {
+                throw Abort(.notFound, reason: "No deck found with id \(id)")
+            }
+
+            return deck!.toNameJson()
+        }
+    }
+
+    func deleteDeckHandler(_ request: Request) throws -> Future<HTTPStatus> {
+        let user = try request.requireAuthenticated(User.self)
+        let id = try request.parameters.next(DeckIDParam.self)
+
+        return Deck.find(id, on: request).flatMap { deck in
+            guard deck != nil, try deck!.user == user.requireID() else {
+                throw Abort(.notFound, reason: "No deck found with id \(id)")
+            }
+
+            return deck!.delete(on: request).transform(to: HTTPStatus.noContent)
+        }
     }
 }
