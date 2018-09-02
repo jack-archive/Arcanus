d14f6f3d1e606d82ba9a7ee274e66f4a66fba1d7
Jack Maloney <jmmaloney4@gmail.com>
2018-09-02 12:39:55 -0500

    BothPlayers type

--- Sources/CLI/Game.swift
+++ Sources/CLI/Game.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
+struct BothPlayers<T> {
+    enum Index: Int {
+        case first = 0, second = 1
+    }
+    
+    var storage: [T]
+
+    subscript(index: Index) -> T {
+        get { return storage[index.rawValue] }
+        set { self.storage[index.rawValue] = newValue }
+    }
+    
+    init(_ first: T, _ second: T) {
+        self.storage = [first, second]
+    }
+}
+
 class Game {
     
 }
